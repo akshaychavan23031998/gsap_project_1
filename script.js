256e6d28dc648ea41745e2413674e9f4068b8f7b
Akshay Ram Chavan <111384131+akshaychavan23031998@users.noreply.github.com>
2024-03-13 15:07:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+var tl = gsap.timeline()
+tl.from("#nav h3", {
+    y:-50,
+    opacity:0,
+    duration:0.8,
+    dealy:0.5,
+    stagger:0.3
+}) 
+
+tl.from("#main h1", {
+    x:-500,
+    opacity:0,
+    duration:1,
+    stagger:0.3
+})
+tl.from("img", {
+    x:100,
+    opacity:0,
+    duration:1,
+    stagger:0.5,
+    rotate:45
+})
